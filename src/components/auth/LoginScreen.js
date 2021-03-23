e3bc521c0214409a2b1ede8f114a39eb6022feb1
Jonathan <jadamson382@gmail.com>
2021-03-22 19:46:40 -0500

    v1.0.1 frt

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -17,10 +17,10 @@ export const LoginScreen = () => {
     });
 
     const [ formRegisterValues, handleRegisterInputChange ] = useForm({
-        rName: 'Jonathan Adamson',
-        rEmail: 'jonnhrr88@gmail.com',
-        rPassword1: '123456',
-        rPassword2: '123456'
+        rName: '',
+        rEmail: '',
+        rPassword1: '',
+        rPassword2: ''
     });
     
     const { lEmail, lPassword } = formLoginValues;
